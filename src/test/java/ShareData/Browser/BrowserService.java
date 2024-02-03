01f78a3878188a7f8294fcd5675fc7480a0d4d76
alinst24 <alinstanciu707@yahoo.com>
2024-02-03 10:54:06 +0200

    Few comments added

--- src/test/java/ShareData/Browser/BrowserService.java
+++ src/test/java/ShareData/Browser/BrowserService.java
@@ -5,7 +5,9 @@ public interface BrowserService {
 
     void openBrowser(Boolean cicd);
     void closeBrowser();
-    Object prepareBrowserOptions(Boolean cicd);
+    Object prepareBrowserOptions(Boolean cicd);          // daca vrem sa apelam pentru un browser ca este chrome,edge sau firefox (ar fi trebuit sa facem 3 metode aici si fiecare metoda sa fie cu un tip diferit, dar nu putem face asa pentru ca daca facem metode abstracte intr-o interfata ne forteaza sa le implementam si atunci trebuie sa apelam la un obiect de tipul generic "Object")
+                                             // cu chestia asta ce castigam : ex: in ChromeBrowserService , linia 34 am facut un public object si sus de tot in metoda in care o folosim i-am facut un "Cast" la linia 17 : Adica am apelat-o si am pus in paranteza ChromeOptions = tipul obiectului sa fie ChromeOptions
+                            // si astfel, orice fel de obiect vrem sa ii dam acolo, nu trebuie sa ii specificam tipul in mod direct (ii spunem doar ca trebuie sa fie un obiect) si el mai departe stie sa se descurce(l-am generalizat)
 
 
 
